d236d40c60f57b43556a87c58533e045bd81015d
Santoshdandin <santusb427@gmail.com>
2022-02-05 00:36:25 +0530

    quiz completed

--- quiz.js
+++ quiz.js
@@ -1,1 +1,21 @@
-const submitAnswer = document.querySelector("#submit-answer-btn");
+const quizForm = document.querySelector(".quiz-form")
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const outputE1 = document.querySelector('#output');
+
+const correctAnswers = ["90°", "Right angled"];
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formResults = new FormData(quizForm);
+    for(let value of formResults.values()){
+        if(value === correctAnswers[index]){
+            score = score+1;
+        }
+        index = index+1;
+    }
+   outputE1.innerText = "Your score is " + score;
+}
+
+
+submitAnswerBtn.addEventListener('click', calculateScore);
