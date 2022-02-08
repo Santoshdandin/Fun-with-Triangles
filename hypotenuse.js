1f0460c4185f2815751062ad6183cebe70ac0aee
Santoshdandin <santusb427@gmail.com>
2022-02-08 10:07:13 +0530

    initial js

---
+++ hypotenuse.js
@@ -0,0 +1,9 @@
+const sides = document.querySelectorAll(".side-input");
+const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
+const outputE1 = document.querySelector("#output");
+
+
+
+
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);
