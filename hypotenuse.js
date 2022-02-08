a0521f7ebc35e49b914c0bd28f24b69d52c12b35
Santoshdandin <santusb427@gmail.com>
2022-02-08 11:04:55 +0530

    squareroot

--- hypotenuse.js
+++ hypotenuse.js
@@ -3,7 +3,17 @@ const hypotenuseBtn = document.querySelector("#hypotenuse-btn");
 const outputE1 = document.querySelector("#output");
 
 
+function calculateSumOfSquares(a,b) {
+    const sumOfSquares = a*a+b*b;
+    // console.log(sumOfSqures);
+    return sumOfSquares;
+}
 
 
+function calculateHypotenuse() {
+    const sumOfSquares = calculateSumOfSquares(Number(sides[0].value),Number(sides[1].value));
+    console.log(sumOfSquares);
+}
 
-hypotenuseBtn.addEventListener("click", calculateHypotenuse);
+
+hypotenuseBtn.addEventListener("click", calculateHypotenuse);1
