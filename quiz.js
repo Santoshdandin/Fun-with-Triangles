01303ced8eb69917a28b38598d5ba330b0dc5a82
Santoshdandin <santusb427@gmail.com>
2022-02-12 00:05:18 +0530

    quiz css

--- quiz.js
+++ quiz.js
@@ -2,7 +2,18 @@ const quizForm = document.querySelector(".quiz-form")
 const submitAnswerBtn = document.querySelector("#submit-answer-btn");
 const outputE1 = document.querySelector('#output');
 
-const correctAnswers = ["90°", "Right angled"];
+const correctAnswers = [
+    "90°",
+    "right angled",
+    "one right angle",
+    "12, 16, 20",
+    "Equilateral triangle",
+    "100°",
+    "30°",
+    "a + b + c",
+    "no",
+    "45°"
+  ];
 
 function calculateScore() {
     let score = 0;
